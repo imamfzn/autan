5d5cb04bc8b7911ed386af6143e80dad0c971202
Imam Fauzan <imamfzn@gmail.com>
2021-02-13 21:57:26 +0700

    fix status code

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -5,7 +5,7 @@ async function login(req, res, next){
     const { username, password } = req.body;
     const { user, token } = await AuthService.login(username, password);
 
-    return res.status(201).json({ ...user, token });
+    return res.json({ ...user, token });
   } catch (e) {
     return next(e);
   }
