b410faa1fda5497b3ed7ebf06667474196906840
Imam Fauzan <imamfzn@gmail.com>
2021-02-13 22:17:44 +0700

    revamp register error

--- src/services/user/register.js
+++ src/services/user/register.js
@@ -2,16 +2,27 @@ const bcrypt = require("bcrypt");
 const User = require('../../models/user');
 
 async function register({username, password, role}){
-  const userExists = await User.findOne({ username });
-  if (userExists) {
-    const error = new Error('username already taken.');
-    error.statusCode = 409;
-    throw error;
+  const userRegister = new User({ username, role });
+
+  try {
+    userRegister.password = await bcrypt.hash(password, 10);
+  } catch (err) {
+    console.error(err);
+    throw new Error("something wrong, can't create new user.");
   }
 
-  const userRegister = new User({username, role});
-  userRegister.password = await bcrypt.hash(password, 10);
-  await userRegister.save();
+  try {
+    await userRegister.save();
+  } catch (err) {
+    if (err.code == 11000){
+      const error = new Error('username already taken.');
+      error.statusCode = 409;
+      throw error;
+    }
+
+    console.error(err);
+    throw new Error("something wrong, can't create new user.");
+  }
 
   const user = userRegister.toObject();
   delete user.password;
