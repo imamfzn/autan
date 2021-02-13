73f9d5853c666efd1d0a773c815b96043a75b7ba
Imam Fauzan <imamfzn@gmail.com>
2021-02-13 20:31:10 +0700

    move basic auth to separate file middleware

---
+++ src/middlewares/basic_auth.js
@@ -0,0 +1,7 @@
+const basicAuth = require('express-basic-auth')
+
+module.exports = basicAuth({
+  users: {
+    [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD,
+  }
+});
--- src/routes/internal/index.js
+++ src/routes/internal/index.js
@@ -1,16 +1,8 @@
 const { Router } = require('express');
-const basicAuth = require('express-basic-auth')
 
 const route = Router();
 
-route.use(basicAuth(
-  {
-    users: {
-      [process.env.BASIC_AUTH_USER]: process.env.BASIC_AUTH_PASSWORD,
-    }
-  }
-));
-
+route.use(require('../../middlewares/basic_auth'));
 route.use('/users', require('./user'));
 
 module.exports = route;
